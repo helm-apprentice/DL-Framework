dd9fdb29ec17ea4454e7f03dc4ee271b26b34427
Kai Li <127737779+helm-apprentice@users.noreply.github.com>
2023-09-11 17:03:03 +0800

    Update __init__.py
    pack up

--- dl/__init__.py
+++ dl/__init__.py
@@ -1,1 +1,15 @@
+is_simple_core = True
 
+if is_simple_core:
+    from dl.core_simple import Variable
+    from dl.core_simple import Function
+    from dl.core_simple import using_config
+    from dl.core_simple import no_grad
+    from dl.core_simple import as_array
+    from dl.core_simple import as_variable
+    from dl.core_simple import setup_variable
+
+else:
+    pass
+
+setup_variable()
