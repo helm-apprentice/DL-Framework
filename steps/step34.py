4976df887d539eacd5a0a0e9d99fc70044524a08
Kai Li <127737779+helm-apprentice@users.noreply.github.com>
2023-09-19 14:01:30 +0800

    Update step34.py

--- steps/step34.py
+++ steps/step34.py
@@ -1,1 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dl import Variable
+import dl.functions as F
 
+x = Variable(np.linspace(-7, 7, 200))
+y = F.sin(x)
+y.backward(create_graph=True)
+
+logs = [y.data]
+
+for i in range(3):
+    logs.append(x.grad.data)
+    gx = x.grad
+    x.cleargrad()
+    gx.backward(create_graph=True)
+
+labels = ["y=sin(x)", "y'", "y''", "y'''"]
+for i, v in enumerate(logs):
+    plt.plot(x.data, logs[i], label=labels[i])
+plt.legend(loc='lower right')
+plt.show()
