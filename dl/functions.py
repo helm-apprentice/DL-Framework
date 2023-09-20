3e3f23b6c3980be04b6121c9709b703bf3072926
Kai Li <127737779+helm-apprentice@users.noreply.github.com>
2023-09-20 20:43:26 +0800

    Update functions.py

--- dl/functions.py
+++ dl/functions.py
@@ -26,3 +26,16 @@ class Cos(Function):
     
 def cos(x):
     return Cos()(x)
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+    
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y * y)
+        return gx
+    
+def tanh(x):
+    return Tanh()(x)
