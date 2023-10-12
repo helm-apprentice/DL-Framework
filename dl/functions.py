8cb41d329d1d1ec9c5ad7fc3936af0a4299b7617
Kai Li <127737779+helm-apprentice@users.noreply.github.com>
2023-10-12 21:05:13 +0800

    Update functions.py

--- dl/functions.py
+++ dl/functions.py
@@ -140,3 +140,19 @@ class MatMul(Function):
     
 def matmul(x, W):
     return MatMul()(x, W)
+
+class MeanSquaredError(Function):
+    def forward(self, x0, x1):
+        diff = x0 - x1
+        y = (diff ** 2).sum() / len(diff)
+        return y
+    
+    def backward(self, gy):
+        x0 ,x1 = self.inputs
+        diff = x0 -x1
+        gx0 = gy * diff * (2. /len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+    
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
