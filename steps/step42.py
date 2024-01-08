0fb48e30300242ed049e3026019dbbfab69c8459
Kai Li <127737779+helm-apprentice@users.noreply.github.com>
2024-01-08 20:40:20 +0800

    Update step42.py

--- steps/step42.py
+++ steps/step42.py
@@ -11,6 +11,23 @@ x, y = Variable(x), Variable(y)
 W = Variable(np.zeros((1, 1)))
 b = Variable(np.zeros(1))
 
-def predict(x):
+def predict(x):        
     y = F.matmul(x, W) + b
     return y
+def mean_squared_error(x0, x1):
+    diff = x0 - x1
+    return F.sum(diff**2)/len(diff)
+
+lr = 0.1
+iters = 100
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = mean_squared_error(y, y_pred)
+    W.cleargrad()
+    b.cleargrad()
+    loss.backward()
+    W.data -= lr * W.grad.data
+    b.data -= lr * b.grad.data
+
+    print('W: ', W.data, 'b: ', b.data, 'loss: ', loss.data)
